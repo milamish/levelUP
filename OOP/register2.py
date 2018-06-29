ec78a764322cc06e8877bef7cdf1dc0d792049cf
milamish <milamish@live.com>
2018-06-29 11:56:11 +0300

    still trying to work on OOp

---
+++ OOP/register2.py
@@ -0,0 +1,13 @@
+from andela import Users
+from login2 import user_login
+class user_register:
+    pass
+def NoRecords(self):
+    self.username = input("enter your username: ")
+ 
+    if self.username in users:
+        print("\nLogin name already exist!\n")
+    else:
+        self.emailadress=input("emailadress")
+        self.password = input("Enter password: ")
+        Users[Username] =Password
